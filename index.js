94739a3942a248afc4283229ea5fba2e9ceda3ef
Carlos Gonzalez <cyberpolin@gmail.com>
2017-12-26 20:31:28 -0600

    remove extra lines, and add react

--- index.js
+++ index.js
@@ -1,11 +1,5 @@
 import React from 'react'
 
-<div id="google_translate_element"></div><script type="text/javascript">
-function googleTranslateElementInit() {
-  new google.translate.TranslateElement({pageLanguage: 'en', autoDisplay: false}, 'google_translate_element');
-}
-</script><script type="text/javascript" src="//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"></script>
-        
 const dynamicScript = ({src, async}) => {
   const script = document.createElement('script')
   script.async = async
@@ -13,13 +7,14 @@ const dynamicScript = ({src, async}) => {
   document.getElementByTag('body').appendChild(script)
 }
 
-class GoogleTranslate extends (React.component){
+class GoogleTranslate extends (React.component) {
   componentWillMount () {
     const src = '//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit'
     dynamicScript({src, async: false, cb: 'googleTranslateElementInit' })
   }
   render () {
-    return <div id="google_translate_element"></div>
+    return <div id='google_translate_element' />
   }
-
 }
+
+export GoogleTranslate
